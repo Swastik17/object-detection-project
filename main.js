d138d4e91a8fd7ae41cf4825a5fb8e391f260c78
Swastik17 <74951457+Swastik17@users.noreply.github.com>
2021-03-30 11:52:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function screena(){
+    window.location="screen1.html";
+}
+function screenb(){
+    window.location="screen2.html";
+}
+function screenc(){
+    window.location="screen3.html";
+}
+function screend(){
+    window.location="screen4.html";
+}
